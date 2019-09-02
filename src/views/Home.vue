4c9da78b9dc474dafa3e5ef9c85d0042140958dc
jixinqi <577591592@qq.com>
2019-09-02 16:37:37 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{$store.state.count}}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  created () {
+    console.log(this)
   }
 }
 </script>
